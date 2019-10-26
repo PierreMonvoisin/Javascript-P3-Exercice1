e3ed800441653a1dd21f33bafa4355c062463d50
Pierre Monvoisin <monvoisin.pierre@gmail.com>
2019-10-26 12:35:26 +0200

    Exercice 1 terminé

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+let image = document.getElementById("image1");
+
+image.onmouseenter = function() {
+  image.style.border = "3px solid red";
+};
+image.onmouseout = function() {
+  image.style.border = "0";
+};
